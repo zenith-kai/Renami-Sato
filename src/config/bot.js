5bc6689f06dbc9db38f6e1d6f6f79ddf2eabc5fe
zenith-kai <mahinaychunvic@gmail.com>
2026-05-04 23:13:46 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Ego Project Assistant",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
